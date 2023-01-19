a652a6d95653339b9e58eb1998055313860418c4
MathieuFigari <mathieu.figari@gmail.com>
2023-01-19 14:14:05 +0100

    update data

--- data/data.sql
+++ data/data.sql
@@ -1,29 +1,18 @@
-DROP TABLE IF EXISTS "movies"; 
-DROP TABLE IF EXISTS "series";
+DROP TABLE IF EXISTS "videos"; 
 DROP TABLE IF EXISTS "genres";
 DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "videos_genres";
 
 
 
-
-CREATE TABLE IF NOT EXISTS "movies"(
+CREATE TABLE IF NOT EXISTS "videos"(
     "id" SERIAL PRIMARY KEY,
     "title" VARCHAR(50) NOT NULL,
     "date" DATE NOT NULL,
     "description" VARCHAR(400) NOT NULL,
     "picture" VARCHAR(500),
-    "video" VARCHAR(500)
-   
-);
-
-
-CREATE TABLE IF NOT EXISTS "series"(
-    "id" SERIAL PRIMARY KEY,
-    "title" VARCHAR(50) NOT NULL,
-    "date" DATE NOT NULL,
-    "description" VARCHAR(400) NOT NULL,
-    "picture" VARCHAR(500),
-    "video" VARCHAR(500)
+    "video" VARCHAR(500),
+    "category" VARCHAR(50)
       
 );
 
@@ -42,21 +31,14 @@ CREATE TABLE IF NOT EXISTS "genres"(
 );
 
 
-CREATE TABLE IF NOT EXISTS "movies_genres"(
+CREATE TABLE IF NOT EXISTS "videos_genres"(
     "movie_id" INT NOT NULL,
     "genre_id" INT NOT NULL,
-    FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (video_id) REFERENCES videos (id) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    PRIMARY KEY (movie_id, genre_id)
+    PRIMARY KEY (video_id, genre_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS "series_genres"(
-    "serie_id" INT NOT NULL,
-    "genre_id" INT NOT NULL,
-    FOREIGN KEY (serie_id) REFERENCES series (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    PRIMARY KEY (serie_id, genre_id)
-);
 
 
