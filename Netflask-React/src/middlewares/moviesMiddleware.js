d84c5b7918b2cd1fa4dead9aa79a094b24454e95
Kortoridon <sylvaincoulay@gmail.com>
2023-01-26 10:39:45 +0100

    test

--- Netflask-React/src/middlewares/moviesMiddleware.js
+++ Netflask-React/src/middlewares/moviesMiddleware.js
@@ -1,17 +1,53 @@
+import { FETCH_GENRES, FETCH_ONE_GENRE, saveGenres, saveOneGenre } from "../actions/moviesActions";
+import axios from "axios";
+
 const moviesMiddleware = (store => (next) => (action) => {
 
-switch (action.type) {
+    switch (action.type) {
+        case FETCH_GENRES: {
 
-    // Requetes API à effectuer en fonction de l'action déclanchée lorsque on voudra utiliser notre API 
+            axios.get(`http://localhost:8080/genres`)
+            .then ( 
+            (response) =>{
+                store.dispatch(saveGenres(response.data));
+            }).catch(
+                (error) => console.error(error),
+            );
+
+            next(action);
+            break;
+            
+        }
 
 
+        case FETCH_ONE_GENRE: {
+
+            const id = action.genreId
+            
+
+            axios.get(`http://localhost:8080/genre/${id}`)
+            .then (
+                (response) => {
+                    store.dispatch(saveOneGenre(response.data));
+                }).catch(
+                    (error) => console.error(error)
+                );
 
 
-    default:
-    next(action)
-}
+            next(action);
+            break;
 
 
+        }
+
+
+      
+
+        default:
+            next(action)
+    }
 
 
 })
+
+export default moviesMiddleware;
